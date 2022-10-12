7d027fcb723b0eb67c4a73ccaea9cbebc4e46ffc
Neuy <neuwirth.andrew9@gmail.com>
2022-10-11 21:58:23 -0400

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Sets up express
 const express = require('express');
 const app = express()
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -14,9 +14,9 @@ const htmlRoutes = require('./routes/htmlRoutes')
 app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
 
-app.listen(port, err => {
+app.listen(PORT, err => {
     if(err) {
         return console.log('ERROR', err);
     }
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${PORT}`);
 });
